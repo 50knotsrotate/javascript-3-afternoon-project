28d7f5f030ab18f59216352b5ffe6e7896af2ef8
= <=>
2019-03-06 14:56:55 -0700

    did problems

--- spread.js
+++ spread.js
@@ -15,13 +15,12 @@ let product = {
   name: 'santa hat',
   price: 6,
   imageUrl: 'https://i.ebayimg.com/images/g/9T4AAOSwbmdZf2jN/s-l640.jpg'
-}
+};
 
-var productCopy = {
+let productCopy = {
   ...product
-}
-
-
+};
+//console.log(productCopy)
 
 
 ////////// PROBLEM 2 //////////
@@ -30,7 +29,7 @@ var productCopy = {
 
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-// code here
+let colorsCopy = [...colors]
 
 
 
@@ -45,7 +44,7 @@ function addNums(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
-// code here
+var result = addNums(...numbers)
 
 
 ////////// PROBLEM 4 //////////
@@ -55,4 +54,5 @@ function addNums(num1, num2, num3) {
 let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
-// code here
+
+combinedArrays = [...arr1, ...arr2]
